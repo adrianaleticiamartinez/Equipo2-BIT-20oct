6698538ea78886ac8a5522d5507c04551ec3a8c1
Gabene <72827905+Gabene@users.noreply.github.com>
2020-10-20 12:30:22 -0500

    Add files via upload
    API en node.js

---
+++ employees.js
@@ -0,0 +1,48 @@
+const express = require ('express');
+const router = express.Router();
+
+const mysqlConnection = require('../database');
+
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM employees', (err, rows, fields) => {
+        if(!err) {
+            res.json(rows );
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err) {
+            res.json(rows[0]);
+        } else {
+            console.log(err); 
+         }
+    }); 
+});
+
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT id, nombre, sexo, segmento, cuenta FROM employees', (err, rows, fields) => {
+        if(!err) {
+            res.json(rows );
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT id, nombre, sexo, segmento, cuenta FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err) {
+            res.json(rows[0]);
+        } else {
+            console.log(err); 
+         }
+    }); 
+});
+
+module.exports = router;
